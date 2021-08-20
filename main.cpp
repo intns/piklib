0d8b8c8e07e6609e46d70ae253b3484a6456b4e1
intns <84647527+intns@users.noreply.github.com>
2021-08-20 19:02:55 +0100

    Close files after reading

--- main.cpp
+++ main.cpp
@@ -6,21 +6,25 @@ int main(int argc, char** argv)
     util::fstream_reader dca_reader(dca_file, 0, util::fstream_reader::Endianness::Big);
     piklib::DCA dca;
     dca.read(dca_reader);
+    dca_file.close();
 
     std::ifstream dck_file("unit-test/pellet1.dck", std::ios_base::binary);
     util::fstream_reader dck_reader(dck_file, 0, util::fstream_reader::Endianness::Big);
     piklib::DCK dck;
     dck.read(dck_reader);
+    dck_file.close();
 
     std::ifstream bti_file("unit-test/z_btn.bti", std::ios_base::binary);
     util::fstream_reader bti_reader(bti_file, 0, util::fstream_reader::Endianness::Big);
     piklib::BTI bti;
     bti.read(bti_reader);
+    bti_file.close();
 
     std::ifstream txe_file("unit-test/rootRing.txe", std::ios_base::binary);
     util::fstream_reader txe_reader(txe_file, 0, util::fstream_reader::Endianness::Big);
     piklib::TXE txe;
     txe.read(txe_reader);
+    txe_file.close();
 
     return 0;
 }
